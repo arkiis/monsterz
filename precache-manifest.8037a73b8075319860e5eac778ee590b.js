bb7d33f06553fc6853a4fc9e6b21c6c310693f5a
Your Name <william.fleckenstein@gmail.com>
2019-07-20 13:59:48 -0500

    Updates

---
+++ precache-manifest.8037a73b8075319860e5eac778ee590b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3212aababe8f4a52ff2cbe5e9ee3ea24",
+    "url": "/arkiis/monsterz/index.html"
+  },
+  {
+    "revision": "8d16fba3e1b3527e28bf",
+    "url": "/arkiis/monsterz/static/css/main.a5a77cd6.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/arkiis/monsterz/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "8d16fba3e1b3527e28bf",
+    "url": "/arkiis/monsterz/static/js/main.c967c0d7.chunk.js"
+  },
+  {
+    "revision": "2e6595aac58390138345",
+    "url": "/arkiis/monsterz/static/js/runtime~main.7b2f42e4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/arkiis/monsterz/precache-manifest.8037a73b8075319860e5eac778ee590b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arkiis/monsterz/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
